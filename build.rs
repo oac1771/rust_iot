97fbcfd58ac353d0e3228855cadfba247bdd1312
Omar Carey <oac1771@gmail.com>
2025-11-08 20:21:54 -0800

    working bluetooth

--- build.rs
+++ build.rs
@@ -1,8 +1,3 @@
 fn main() {
-    let ssid = std::env::var("SSID").unwrap_or_else(|_| "ssid".to_string());
-    let password = std::env::var("PASSWORD").unwrap_or_else(|_| "password".to_string());
-
     println!("cargo:rustc-link-arg=-Tlinkall.x");
-    println!("cargo:rustc-env=SSID={}", ssid);
-    println!("cargo:rustc-env=PASSWORD={}", password);
 }
